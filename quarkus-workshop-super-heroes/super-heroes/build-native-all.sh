81052e5f6bb0d4f3679d9b6cc1efecf5bd49efb1
Thomas Bühren <16827920+buehren@users.noreply.github.com>
2020-12-18 19:34:04 +0100

    Add script for building all native executables (stops in case of a failure)

---
+++ quarkus-workshop-super-heroes/super-heroes/build-native-all.sh
@@ -0,0 +1,13 @@
+cd rest-hero && \
+mvn clean package -Pnative -Dnative-image.docker-build=true -DskipTests && \
+cd .. && \
+cd rest-villain && \
+mvn clean package -Pnative -Dnative-image.docker-build=true -DskipTests && \
+cd .. && \
+cd rest-fight && \
+cp -R ../ui-super-heroes/dist/* src/main/resources/META-INF/resources && \
+mvn clean package -Pnative -Dnative-image.docker-build=true -DskipTests && \
+cd .. && \
+cd event-statistics && \
+mvn clean package -Pnative -Dnative-image.docker-build=true -DskipTests && \
+cd ..
