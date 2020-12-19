5fa4e40bd20085254e8cb6dd14f44382bffdd7a6
Thomas Bühren <thomas.buehren@ferchau.com>
2020-12-19 01:25:55 +0100

    Add commands for building UI

--- quarkus-workshop-super-heroes/super-heroes/build-native-all.sh
+++ quarkus-workshop-super-heroes/super-heroes/build-native-all.sh
@@ -4,6 +4,11 @@ cd .. && \
 cd rest-villain && \
 mvn clean package -Pnative -Dnative-image.docker-build=true -DskipTests && \
 cd .. && \
+cd ui-super-heroes && \
+mvn install && \
+npm install && \
+./package.sh && \
+cd .. && \
 cd rest-fight && \
 cp -R ../ui-super-heroes/dist/* src/main/resources/META-INF/resources && \
 mvn clean package -Pnative -Dnative-image.docker-build=true -DskipTests && \
