64b4cbaa6b8102ff0f76ef40bc851702d65d1991
Thomas Bühren <thomas.buehren@ferchau.com>
2021-01-29 20:09:47 +0100

    kill native processes with -9 also (one process was stuck)

--- quarkus-workshop-super-heroes/super-heroes/stop-native-all.sh
+++ quarkus-workshop-super-heroes/super-heroes/stop-native-all.sh
@@ -3,4 +3,6 @@
 # To also stop the infrastructure services:
 #     ./stop-infrastructure.sh
 
-pkill -e -f ".*target/.*runner.*"
+pkill    -e -f ".*target/.*runner.*"
+sleep 2
+pkill -9 -e -f ".*target/.*runner.*"
