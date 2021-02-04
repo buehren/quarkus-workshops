4cfc1488a5024ce54014bab1b2222fb46a020cdb
Thomas Bühren <thomas.buehren@ferchau.com>
2021-02-04 21:47:06 +0100

    Fix patterns for stopping services

--- quarkus-workshop-super-heroes/super-heroes/stop-jars-all.sh
+++ quarkus-workshop-super-heroes/super-heroes/stop-jars-all.sh
@@ -8,10 +8,10 @@ function run
     source superhero-services-env.sh || return
 
     for service in $SUPERHERO_SERVICES ui-super-heroes; do
-        pgrep -af "java -jar $service/target/$service-01-runner\.jar"
+        pgrep -af "java .*-jar $service/target/$service-01-runner\.jar"
         if [ "$?" == "0" ]; then
             echo "======================================= KILL JAR: $service ======================================= " && \
-            pkill -ef "java -jar $service/target/$service-01-runner\.jar" || return
+            pkill -ef "java .*-jar $service/target/$service-01-runner\.jar" || return
         fi
     done
 
--- quarkus-workshop-super-heroes/super-heroes/stop-native-all.sh
+++ quarkus-workshop-super-heroes/super-heroes/stop-native-all.sh
@@ -8,25 +8,25 @@ function run
     source superhero-services-env.sh || return
 
     for service in $SUPERHERO_SERVICES; do
-        pgrep -af "$service/target/$service-01-runner[^.]"
+        pgrep -af "$service/target/$service-01-runner[^.]*"
         if [ "$?" == "0" ]; then
             echo "======================================= KILL NATIVE: $service ======================================= " && \
-            pkill -ef "$service/target/$service-01-runner[^.]" || return
+            pkill -ef "$service/target/$service-01-runner[^.]*" || return
         fi
     done
     sleep 2
     for service in $SUPERHERO_SERVICES; do
-        pgrep -af "$service/target/$service-01-runner[^.]"
+        pgrep -af "$service/target/$service-01-runner[^.]*"
         if [ "$?" == "0" ]; then
             echo "======================================= KILL -9 NATIVE: $service ======================================= " && \
-            pkill -9 -ef "$service/target/$service-01-runner[^.]"  || return
+            pkill -9 -ef "$service/target/$service-01-runner[^.]*"  || return
         fi
     done
 
     sleep 2
     echo "Services still running:"
     pgrep -af "target/.*-01-runner.*"
-    
+
     return 0
 }
 
