c9c68757ee7bff6dfb5a5afca7b565d3028744fb
Thomas Bühren <thomas.buehren@ferchau.com>
2021-02-25 09:45:18 +0100

    Workaround: Create directory required by mvn-all.sh

--- quarkus-workshop-super-heroes/super-heroes/clean-all.sh
+++ quarkus-workshop-super-heroes/super-heroes/clean-all.sh
@@ -4,6 +4,7 @@ function run
 {
     source superhero-services-env.sh || return
 
+    mkdir -p ui-super-heroes/dist/super-heroes/tmp # because mvn-all.sh accesses this directory for rest-fight
     SUPERHERO_SERVICES="$SUPERHERO_SERVICES ui-super-heroes" ./mvn-all.sh "CLEANING" "clean" "$MAVEN_EXTRA_ARGS" || return
 }
 
--- quarkus-workshop-super-heroes/super-heroes/ui-super-heroes/package.sh
+++ quarkus-workshop-super-heroes/super-heroes/ui-super-heroes/package.sh
@@ -3,6 +3,7 @@
 export DEST=src/main/resources/META-INF/resources
 ./node_modules/.bin/ng build --prod --base-href "."
 rm -Rf ${DEST}
+mkdir -p ${DEST}
 cp -R dist/* ${DEST}
 # end::adocShell[]
 
