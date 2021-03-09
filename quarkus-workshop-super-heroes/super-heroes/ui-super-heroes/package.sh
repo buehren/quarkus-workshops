e1888fe93fbb9f4af771b30b8da8fcbdcee4a49e
Thomas Bühren <thomas.buehren@ferchau.com>
2021-03-09 02:54:00 +0100

    Stop and return error code on failure

--- quarkus-workshop-super-heroes/super-heroes/ui-super-heroes/package.sh
+++ quarkus-workshop-super-heroes/super-heroes/ui-super-heroes/package.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
-# tag::adocShell[]
-export DEST=src/main/resources/META-INF/resources
-./node_modules/.bin/ng build --prod --base-href "."
-rm -Rf ${DEST}
-mkdir -p ${DEST}
-cp -R dist/* ${DEST}
-# end::adocShell[]
 
+function run
+{
+    # tag::adocShell[]
+    ./node_modules/.bin/ng build --prod --base-href "." || return
+
+    export DEST=src/main/resources/META-INF/resources
+    rm -Rf ${DEST}  || return
+    mkdir -p ${DEST} || return
+    cp -R dist/* ${DEST} || return
+    # end::adocShell[]
+}
+
+run "$1" || ( echo "An ERROR occured!"; false )
