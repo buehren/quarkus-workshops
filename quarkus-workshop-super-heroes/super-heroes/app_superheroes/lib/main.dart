19aab19d65252c98d434a22706a1b037bcd0b486
Thomas Bühren <thomas.buehren@ferchau.com>
2021-03-09 21:30:09 +0100

    Add Firebase Auth for Android

--- quarkus-workshop-super-heroes/super-heroes/app_superheroes/lib/main.dart
+++ quarkus-workshop-super-heroes/super-heroes/app_superheroes/lib/main.dart
@@ -8,12 +8,87 @@ import 'package:flutter/material.dart';
 import 'package:web_socket_channel/web_socket_channel.dart';
 import 'package:web_socket_channel/io.dart';
 
+import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_auth_ui/flutter_auth_ui.dart';
 
 import 'package:app_superheroes/winner_score.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(App());
+}
+
+class App extends StatefulWidget {
+  _AppState createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  // Set default `_initialized` and `_error` state to false
+  bool _initialized = false;
+  bool _error = false;
+
+  // Define an async function to initialize FlutterFire
+  void initializeFlutterFire() async {
+    try {
+      // Wait for Firebase to initialize and set `_initialized` state to true
+      await Firebase.initializeApp();
+      setState(() {
+        _initialized = true;
+      });
+    } catch(e) {
+      print(e);
+
+      // Set `_error` state to true if Firebase initialization fails
+      setState(() {
+        _error = true;
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    initializeFlutterFire();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // Show error message if initialization failed
+    if(_error) {
+      return SomethingWentWrong();
+    }
+
+    // Show a loader until FlutterFire is initialized
+    if (!_initialized) {
+      return Loading();
+    }
+
+    return MyApp();
+  }
+}
+
+class Loading extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    final title = 'Super Heroes';
+    return MaterialApp(
+      home: const Text("Loading"),
+    );
+  }
+}
+
+class SomethingWentWrong extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    final title = 'Super Heroes';
+    return MaterialApp(
+        home: const Text("Something went wrong"),
+    );
+  }
+}
 
 class MyApp extends StatelessWidget {
 
