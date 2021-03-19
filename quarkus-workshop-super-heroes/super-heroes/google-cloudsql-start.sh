f3bfe8235248eb221b862eafaa5178513933ec8d
Thomas Bühren <thomas.buehren@ferchau.com>
2021-03-19 23:40:27 +0100

    Remove public IP address when stopping DB instance to avoid costs for unused IP reservation

--- quarkus-workshop-super-heroes/super-heroes/google-cloudsql-start.sh
+++ quarkus-workshop-super-heroes/super-heroes/google-cloudsql-start.sh
@@ -5,7 +5,8 @@ function run
     source ./google-cloudrun-env.sh || return
     #source ./google-cloudsql-env.sh || return
 
-    gcloud sql instances patch "$GCLOUD_DB_INSTANCE" --activation-policy ALWAYS || return
+    # Start database instance and assign public IP address
+    gcloud sql instances patch "$GCLOUD_DB_INSTANCE" --activation-policy ALWAYS --assign-ip || return
 
     gcloud sql instances describe "$GCLOUD_DB_INSTANCE" || return
 }
